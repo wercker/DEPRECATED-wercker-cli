507531a77cba1ae143e14bb0c1e3e9627ba39794
Jacco Flenter <github@putpixel.nl>
2013-01-29 14:46:18 +0100

    now accepts args

--- werckercli/decorators.py
+++ werckercli/decorators.py
@@ -2,14 +2,10 @@ from werckercli.base import get_access_token
 
 
 def login_required(f):
-    def new_f():
-        # print "Entering", f.__name__
-        # print "entering"
+    def new_f(*args, **kwargs):
         token = get_access_token()
-        print token
         if token:
-            f(valid_token=token)
-        # print "Exited", f.__name__
+            return f(valid_token=token, *args, **kwargs)
 
     new_f.__name__ = f.__name__
     return new_f
