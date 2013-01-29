dc324a823256281723a9771be588e0b98ab7a336
Jacco Flenter <github@putpixel.nl>
2013-01-29 14:46:48 +0100

    login_required decorator tests

--- werckercli/tests/test_decorators.py
+++ werckercli/tests/test_decorators.py
@@ -1,8 +1,39 @@
 
 from werckercli.decorators import login_required
 
+from werckercli.tests import (
+    BasicClientCase,
+)
 
-class LoginRequiredTests():
 
-    def test_things(self):
-        login_required
+class LoginRequiredTests(BasicClientCase):
+    template_name = "home-with-token"
+
+    def test_valid_token(self):
+
+        @login_required
+        def nothing(valid_token=None):
+            self.assertEqual(self.valid_token, valid_token)
+
+        nothing()
+
+    def test_return_value(self):
+
+        @login_required
+        def nothing(valid_token=None):
+
+            return valid_token
+
+        result = nothing()
+        self.assertEqual(self.valid_token, result)
+        # print result
+
+    def test_argument_value(self):
+
+        test_string = "test_string_1"
+
+        @login_required
+        def nothing(arg1, valid_token=None):
+            self.assertEqual(arg1, test_string)
+
+        nothing(test_string)
