e9c88ec9afa637e886fe7a7a37f79522058f8c32
Jacco Flenter <github@putpixel.nl>
2013-01-30 14:45:31 +0100

    docopt rewrite...:)

--- werckercli/cli.py
+++ werckercli/cli.py
@@ -1,6 +1,3 @@
-import argparse
-import textwrap
-
 from clint.textui import colored
 
 from werckercli.commands.clearsettings import clear_settings\
@@ -19,62 +16,10 @@ def get_intro():
     return intro
 
 
-def get_parser():
-    command_choices = {
-        "create": "Add a new project to Wercker",
-        "list": "List all deploy targets",
-        "deploy": "Deploy to a specific target",
-        "status": "Gets the most recent build status of an application",
-        "clear-settings":
-        "Removes the login token for wercker from this computer",
-        "help": "Displays this text and exits"
-    }
+def handle_commands(args):
 
-    description = """wercker-cli for easy commandline access to wercker.
-
-    Commands:"""
-
-    max_length = 0
-    for command in command_choices:
-        if len(command) > max_length:
-            max_length = len(command)
-
-    for command in command_choices:
-        description += \
-            """\n    """ + \
-            command.ljust(max_length) + \
-            """ : """ + \
-            command_choices[command]
-
-    description += "\n"
-
-    parser = argparse.ArgumentParser(
-        description=textwrap.dedent(description),
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-
-    parser.add_argument(
-        'base_command',
-        metavar='<command>',
-        type=str,
-        help='options are: ' + ', '.join(command_choices.keys()),
-        choices=command_choices.keys(),
-        nargs="?"
-    )
-
-    return parser
-
-
-def handle_commands(parser, args):
-
-    base_command = args.base_command
-
-    if base_command is None:
-        base_command = "help"
-
-    if base_command.lower() == 'help':
-        parser.print_help()
-    if base_command.lower() == 'create':
+    if args['create']:
         command_create()
 
-    elif base_command.lower() == 'clear-settings':
+    elif args['clear-settings']:
         command_clear_settings()
