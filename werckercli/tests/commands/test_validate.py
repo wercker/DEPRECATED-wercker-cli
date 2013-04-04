41b63df85b7b4b62212551cc0e7804cc130bd489
Pieter Joost van de Sande <pj@born2code.net>
2013-04-04 16:40:06 +0200

    Satisfy pip8 and increase version

--- werckercli/tests/commands/test_validate.py
+++ werckercli/tests/commands/test_validate.py
@@ -4,23 +4,24 @@ from werckercli.cli import get_term
 from werckercli.tests import TestCase
 from werckercli.commands.validate import validate
 
+
 class ValidationTests(TestCase):
 
-	@mock.patch("os.path.isfile", mock.Mock(return_value=False))
-	def test_warns_when_wercker_json_not_exists(self):
+    @mock.patch("os.path.isfile", mock.Mock(return_value=False))
+    def test_warns_when_wercker_json_not_exists(self):
 
-		with mock.patch("werckercli.cli.puts", mock.Mock()) as puts:
-			validate()
-	
-			term = get_term()
-			expectedMsg = term.yellow("Warning: ") + "Could not find a wercker.json file"
-			puts.assert_called_with(expectedMsg)
+        with mock.patch("werckercli.cli.puts", mock.Mock()) as puts:
+            validate()
 
-	@mock.patch("os.path.isfile", mock.Mock(return_value=True))
-	def test_puts_error_when_wercker_json_could_not_be_opened(self):
-		e = IOError('foo')
-		with mock.patch("os.open", mock.Mock(side_effect=e)) as open:
-			with mock.patch("werckercli.cli.puts", mock.Mock()) as puts:
-				validate()
-				open.assert_called_once()
-				puts.assert_called_once()
+            term = get_term()
+            m = term.yellow("Warning: ") + "Could not find a wercker.json file"
+            puts.assert_called_with(m)
+
+    @mock.patch("os.path.isfile", mock.Mock(return_value=True))
+    def test_puts_error_when_wercker_json_could_not_be_opened(self):
+        e = IOError('foo')
+        with mock.patch("os.open", mock.Mock(side_effect=e)) as open:
+            with mock.patch("werckercli.cli.puts", mock.Mock()) as puts:
+                validate()
+                open.assert_called_once()
+                puts.assert_called_once()
