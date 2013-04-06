fc87061aaa2096b713c4985edbbb2897bf837f95
Pieter Joost van de Sande <pj@born2code.net>
2013-04-06 12:58:25 +0200

    Introduce path variables for mocking paths

--- werckercli/tests/commands/test_validate.py
+++ werckercli/tests/commands/test_validate.py
@@ -4,12 +4,15 @@ from werckercli.tests import TestCase
 from werckercli.commands.validate import validate
 
 puts_path = "werckercli.commands.validate.puts"
+exists_path = "os.path.exists"
+find_git_root_path = "werckercli.commands.validate.find_git_root"
+getsize_path = "os.path.getsize"
+open_path = "os.open"
 
 
 class ValidationTests(TestCase):
 
-    @mock.patch("werckercli.commands.validate.find_git_root",
-                mock.Mock(return_value=None))
+    @mock.patch(find_git_root_path, mock.Mock(return_value=None))
     def test_errors_when_no_git_repo_exists(self):
         with mock.patch(puts_path, mock.Mock()) as puts:
             validate()
@@ -18,8 +21,8 @@ class ValidationTests(TestCase):
                                    "Could not find a git repository")
             puts.assert_called_with(arg)
 
-    @mock.patch("os.path.exists", mock.Mock(return_value=False))
-    @mock.patch("werckercli.commands.validate.find_git_root",
+    @mock.patch(exists_path, mock.Mock(return_value=False))
+    @mock.patch(find_git_root_path,
                 mock.Mock(return_value="./"))
     def test_warns_when_wercker_json_not_exists(self):
 
@@ -29,13 +32,13 @@ class ValidationTests(TestCase):
             puts.assert_called_with(SubstringMatcher(containing=
                                     "Could not find a wercker.json file"))
 
-    @mock.patch("os.path.exists", mock.Mock(return_value=True))
-    @mock.patch("os.path.getsize", mock.Mock(return_value=42))
-    @mock.patch("werckercli.commands.validate.find_git_root",
+    @mock.patch(exists_path, mock.Mock(return_value=True))
+    @mock.patch(getsize_path, mock.Mock(return_value=42))
+    @mock.patch(find_git_root_path,
                 mock.Mock(return_value="./"))
     def test_puts_error_when_wercker_json_could_not_be_opened(self):
         e = IOError('foo')
-        with mock.patch("os.open", mock.Mock(side_effect=e)) as open_file:
+        with mock.patch(open_path, mock.Mock(side_effect=e)) as open_file:
             with mock.patch(puts_path, mock.Mock()) as puts:
                 validate()
 
@@ -43,13 +46,13 @@ class ValidationTests(TestCase):
                 puts.assert_called_with(SubstringMatcher(
                     containing="Error while reading wercker.json file:"))
 
-    @mock.patch("os.path.exists", mock.Mock(return_value=True))
-    @mock.patch("werckercli.commands.validate.find_git_root",
+    @mock.patch(exists_path, mock.Mock(return_value=True))
+    @mock.patch(find_git_root_path,
                 mock.Mock(return_value="./"))
-    @mock.patch("os.path.getsize", mock.Mock(return_value=0))
+    @mock.patch(getsize_path, mock.Mock(return_value=0))
     def test_puts_error_when_wercker_json_is_empty(self):
         e = IOError('foo')
-        with mock.patch("os.open", mock.Mock(side_effect=e)) as open_file:
+        with mock.patch(open_path, mock.Mock(side_effect=e)) as open_file:
             with mock.patch(puts_path, mock.Mock()) as puts:
                 validate()
 
@@ -57,11 +60,11 @@ class ValidationTests(TestCase):
                 puts.assert_called_with(SubstringMatcher(containing=
                                         "wercker.json is found, but empty"))
 
-    @mock.patch("os.open", mock.Mock(return_value=mock.MagicMock()))
-    @mock.patch("os.path.exists", mock.Mock(return_value=True))
-    @mock.patch("werckercli.commands.validate.find_git_root",
+    @mock.patch(open_path, mock.Mock(return_value=mock.MagicMock()))
+    @mock.patch(exists_path, mock.Mock(return_value=True))
+    @mock.patch(find_git_root_path,
                 mock.Mock(return_value="./"))
-    @mock.patch("os.path.getsize", mock.Mock(return_value=42))
+    @mock.patch(getsize_path, mock.Mock(return_value=42))
     def test_puts_error_when_wercker_json_is_not_valid(self):
         e = ValueError('foo')
         with mock.patch("json.load", mock.Mock(side_effect=e)) as json_load:
@@ -72,11 +75,11 @@ class ValidationTests(TestCase):
                 puts.assert_called_with(SubstringMatcher(containing=
                                         "wercker.json is not valid json: "))
 
-    @mock.patch("os.open", mock.Mock(return_value=mock.MagicMock()))
-    @mock.patch("os.path.exists", mock.Mock(return_value=True))
-    @mock.patch("werckercli.commands.validate.find_git_root",
+    @mock.patch(open_path, mock.Mock(return_value=mock.MagicMock()))
+    @mock.patch(exists_path, mock.Mock(return_value=True))
+    @mock.patch(find_git_root_path,
                 mock.Mock(return_value="./"))
-    @mock.patch("os.path.getsize", mock.Mock(return_value=42))
+    @mock.patch(getsize_path, mock.Mock(return_value=42))
     def test_puts_error_when_wercker_json_is_not_valid(self):
         with mock.patch("json.load", mock.Mock()) as json_load:
             with mock.patch(puts_path, mock.Mock()) as puts:
