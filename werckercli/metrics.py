4f44e975d73e0a55b741bcc5a6dee641ed615c61
Pieter Joost van de Sande <pj@born2code.net>
2013-04-09 13:01:37 +0200

    Move all metrics logic to metrics.py

--- werckercli/metrics.py
+++ werckercli/metrics.py
@@ -1,9 +1,12 @@
 import requests
 import json
 import base64
+import sys
 
 mixpanel_api_url_template = "http://api.mixpanel.com/track/?data=%s&ip=1"
 token = "509041bd3dd934157b1d3d00bb450ebc"
+default_command_name = "help" # used when application is started without any
+                              # argument
 
 def track_command_usage(command_name, arguments=None):
     """ List a command as used """
@@ -17,5 +20,17 @@ def track_command_usage(command_name, arguments=None):
             }}
 
     encoded_data = base64.b64encode(json.dumps(data))
-    response = requests.get(mixpanel_api_url_template % encoded_data)
-    print response.json()
+    requests.get(mixpanel_api_url_template % encoded_data)
+
+
+def track_application_startup():
+    command = "help"
+    arguments = None
+
+    if len(sys.argv) > 2:
+        command = sys.argv[1]
+
+    if len(sys.argv) > 3:
+        arguments = sys.argv[2:]
+
+    track_command_usage(command, arguments)
