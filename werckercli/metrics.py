255c116c963a5e1eeab3c94004d2067b27da2bf3
Pieter Joost van de Sande <pj@born2code.net>
2013-04-09 12:54:12 +0200

    Commands are now the event name and args are included in props

--- werckercli/metrics.py
+++ werckercli/metrics.py
@@ -5,13 +5,15 @@ import base64
 mixpanel_api_url_template = "http://api.mixpanel.com/track/?data=%s&ip=1"
 token = "509041bd3dd934157b1d3d00bb450ebc"
 
-def track_command_usage(command_name):
+def track_command_usage(command_name, arguments=None):
     """ List a command as used """
 
-    data = { "event": "cli command executed",
+    data = { "event": command_name,
              "properties": {
                 "command_name": command_name,
+                "event_source": "cli",
                 "token": token,
+                "arguments": arguments
             }}
 
     encoded_data = base64.b64encode(json.dumps(data))
