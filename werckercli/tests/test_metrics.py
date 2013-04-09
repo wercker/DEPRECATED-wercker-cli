aa299c6637aab429884363216e7f156b2017efd4
Pieter Joost van de Sande <pj@born2code.net>
2013-04-09 19:25:34 +0200

    Adds unit test for silently failing on ConnectionError

---
+++ werckercli/tests/test_metrics.py
@@ -0,0 +1,19 @@
+import mock
+import requests
+from werckercli.metrics import track_application_startup
+
+from werckercli.tests import TestCase
+
+track_command_usage_path = "werckercli.metrics.track_command_usage"
+
+class MetricsTests(TestCase):
+
+    def test_track_application_startup_fails_silently_on_ConnectionError(self):
+        err = requests.ConnectionError()
+        the_method = mock.Mock(side_effect=err)
+
+        with mock.patch(track_command_usage_path, the_method) as puts:
+            try:
+                track_application_startup()
+            except requests.ConnectionError:
+                self.fail("track_application_startup didn't fail silently")
