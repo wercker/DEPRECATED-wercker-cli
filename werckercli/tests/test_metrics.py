4d3b5e5b52e6d8c157986fe7f830ae11cced3198
Pieter Joost van de Sande <pj@born2code.net>
2013-04-10 08:37:58 +0200

    Add test for missing arguments

--- werckercli/tests/test_metrics.py
+++ werckercli/tests/test_metrics.py
@@ -62,3 +62,12 @@ class MetricsTests(TestCase):
 
             expected_args = ['foo', 'bar']
             the_method.assert_called_with(ANY, expected_args)
+
+    @mock.patch("sys.argv", [None, 'validate'])
+    def test_track_application_startup_passes_None_for_missing_arguments(self):
+        the_method = mock.Mock()
+        with mock.patch(track_command_usage_path, the_method) as puts:
+            track_application_startup()
+
+            expected_args = None
+            the_method.assert_called_with(ANY, expected_args)
