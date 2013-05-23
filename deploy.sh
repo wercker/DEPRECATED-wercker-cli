c07748323ba43ad3614e3291c8a7cda407f93f3e
Jacco Flenter <github@putpixel.nl>
2013-05-23 11:37:56 +0200

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-echo "[server-login]" >> ~/.pypirc
-echo "username:" $PYPI_USER > ~/.pypirc
+echo "[server-login]" > ~/.pypirc
+echo "username:" $PYPI_USER >> ~/.pypirc
 echo "password:" $PYPI_PASSWORD >> ~/.pypirc
-cat ~/.pypirc
 python setup.py sdist upload
